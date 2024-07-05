a37eda363ac9a83076f5a811d55cc5974efb3963
princend <waxdji3g4y3c04@gmail.com>
2024-07-05 16:57:47 +0800

    新增首頁

--- blog_post/views.py
+++ blog_post/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from blog_post.models import Post
 # Create your views here.
+
+
+def index(requests):
+    posts = Post.objects.all()
+    post_list = list()
+    for count, post in enumerate(posts):
+        post_list.append("#{}: {} <br><br>".format(str(count), str(post)))
+    return HttpResponse(post_list)
--- djangoBLOG/urls.py
+++ djangoBLOG/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from blog_post.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index),
 ]
