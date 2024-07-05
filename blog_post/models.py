51a99bd97e0342756ab2c5f76b6b36e60574d0e8
princend <waxdji3g4y3c04@gmail.com>
2024-07-05 14:49:15 +0800

    create model

---
+++ blog_post/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+"""_summary_
+Returns:
+    _type_: _description_
+"""
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add = True)
+
+
+    class Meta:
+        ordering = ['-pub_date']
+   
+    def __str__(self):
+        return self.title
+
+"""_summary
+Returns:
+    _type_: _description_
+"""
+class Guestbook(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    message = models.TextField()
+    time = models.DateTimeField(auto_now_add = True)
+        
+    class Meta:
+        ordering = ['-time']
+   
+    def __str__(self):
+        return self.name
