fd9183e6995f34b6365b0a66dcb1c8919574f208
princend <waxdji3g4y3c04@gmail.com>
2024-07-10 15:29:33 +0800

    custom filter

--- djangoBLOG/settings.py
+++ djangoBLOG/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog_post",
+    "blog_post"
 ]
 
 MIDDLEWARE = [
@@ -65,6 +65,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+             "libraries": {                                          # 加入這幾行
+                "markdown_tag": "libraries.filter.markdown_filter", # 加入這幾行
+            }     
         },
     },
 ]
