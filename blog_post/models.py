447a66f903cd017d940b14d5aa61fa545e4ed507
princend <waxdji3g4y3c04@gmail.com>
2024-07-12 13:16:26 +0800

    新增591房屋資訊 table

--- blog_post/models.py
+++ blog_post/models.py
@@ -36,3 +36,32 @@ class Guestbook(models.Model):
    
     def __str__(self):
         return self.name
+
+
+# 591房屋資訊
+class BuildInfo(models.Model):
+    hid = models.IntegerField(unique=True)
+    build_name = models.CharField(max_length=255)
+    cover = models.URLField(max_length=500)
+    price = models.CharField(max_length=50)
+    price_num = models.CharField(max_length=50)
+    price_unit = models.CharField(max_length=50)
+    browsenum = models.CharField(max_length=50)
+    section_name = models.CharField(max_length=100)
+    region_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    callnum = models.IntegerField()
+    area = models.CharField(max_length=50)
+    room = models.CharField(max_length=50)
+    photo_num = models.IntegerField()
+    build_type = models.CharField(max_length=50)
+    build_type_val = models.IntegerField()
+    purpose = models.CharField(max_length=50)
+    purpose_other2_str = models.CharField(max_length=100)
+    tag = models.CharField(max_length=255)
+    layout = models.CharField(max_length=255)
+    build_url = models.URLField(max_length=500)
+    tag_type = models.IntegerField()
+
+    def __str__(self):
+        return self.build_name
