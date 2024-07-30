5279e2f1a780b0e786553be3b7068d064b4c7956
princend <waxdji3g4y3c04@gmail.com>
2024-07-30 17:12:59 +0800

    連接MySql

--- djangoBLOG/settings.py
+++ djangoBLOG/settings.py
@@ -79,12 +79,26 @@ WSGI_APPLICATION = "djangoBLOG.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "USER": "django_db",
+        "NAME": "django_db",
+        "PASSWORD": "f[ahapxsQ!k-AhGH", # 改成自己的密碼
+        "HOST": "127.0.0.1",
+        "PORT": "3306"
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
